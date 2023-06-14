daeada9557f07ef2f161bf98c6d18727742fe2cc
Brandon Tam <tam.brandon29@gmail.com>
2023-06-14 17:11:34 -0400

    add gitignore storybook

--- frontend/storybook-static/assets/preview-cb7002d3.js
+++
@@ -1,2 +0,0 @@
-const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
-//# sourceMappingURL=preview-cb7002d3.js.map
