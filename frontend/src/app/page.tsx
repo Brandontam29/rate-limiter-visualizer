60a459cff6a7223e463df8a8ca2a8523095d45cc
Brandon Tam <tam.brandon29@gmail.com>
2024-03-20 11:25:11 -0400

    can close controls tab

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,16 @@
 import Heading from "@/components/atoms/Heading";
+import Icon from "@/components/atoms/Icon";
 import Visualizer from "@/components/organisms/Visualizer";
 import Controls from "@/sections/Controls";
 import UploadUser from "@/sections/UploadUser";
 import { requestsAtom } from "@/state/requests";
+import cxtw from "@/utils/cxtw";
 import { useAtomValue } from "jotai";
+import { useState } from "react";
 
 const HomePage = () => {
   const requests = useAtomValue(requestsAtom);
-
+  const [showControls, setShowControls] = useState(true);
   return (
     <div className="py-4 lg:py-8 lg:px-8 mx-auto container px-2 grid gap-8 grid-cols-[300px_300px]">
       <UploadUser />
@@ -20,8 +23,23 @@ const HomePage = () => {
         <Visualizer requests={requests} />
       </section>
 
-      <div className="fixed top-0 bottom-0 right-0 bg-background-level-1">
-        <Controls />
+      <div className="fixed right-0 top-1/2 -translate-y-1/2 translate-x-[25px] hover:translate-x-[15px] p-2 transition-translate duration-500">
+        <button
+          type="button"
+          onClick={() => setShowControls(true)}
+          className="rounded-full w-12 aspect-square flex justify-center items-center border border-input-border shadow hover:shadow transition-shadow duration-500"
+        >
+          <span className="sr-only">Open Controls</span>
+          <Icon name="ChevronLeftIcon" size="size-4.5" />
+        </button>
+      </div>
+      <div
+        className={cxtw(
+          "fixed top-0 bottom-0 right-0 bg-background-level-1 transition-transform translate-x-full",
+          showControls && "translate-x-0"
+        )}
+      >
+        <Controls onClose={() => setShowControls(false)} />
       </div>
     </div>
   );
--- frontend/src/sections/Controls.tsx
+++ frontend/src/sections/Controls.tsx
@@ -1,4 +1,5 @@
 import Heading from "@/components/atoms/Heading";
+import Icon from "@/components/atoms/Icon";
 import RadioCardGroup from "@/components/atoms/RadioCardGroup";
 import Toggle from "@/components/atoms/Toggle";
 import { doInfiniteQueryAtom, rateLimiterTypeAtom } from "@/state/controls";
@@ -19,16 +20,24 @@ const RATE_LIMITER_OPTIONS = [
   },
 ] as const;
 
-const Controls = () => {
+type ControlsProps = { onClose: () => void };
+
+const Controls = ({ onClose }: ControlsProps) => {
   const [doInfiniteQuery, setDoInfiniteQuery] = useAtom(doInfiniteQueryAtom);
   const [rateLimiterType, setRateLimiterType] = useAtom(rateLimiterTypeAtom);
 
   console.log({ doInfiniteQuery, rateLimiterType });
   return (
-    <section className="space-y-2 fixed top-0 bottom-0 right-0 bg-background-level-1 p-8">
-      <Heading tag={"h1"} size="size-2xl">
-        Controls
-      </Heading>
+    <section className="space-y-2 bg-background-level-1 p-8">
+      <div className="flex justify-between">
+        <Heading tag={"h1"} size="size-2xl">
+          Controls
+        </Heading>
+        <button type="button" onClick={onClose}>
+          <span className="sr-only">Close Controls</span>
+          <Icon name={"XMarkIcon"} />
+        </button>
+      </div>
 
       <div>
         <Heading tag="h3">Infinite Query</Heading>
